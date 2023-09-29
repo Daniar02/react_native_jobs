69cf234c73c11cc88876bbbdf44c1342547424c2
Daniar02 <abykeev02@icloud.com>
2023-09-29 17:17:50 +0600

    done welcome

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,10 +1,16 @@
-import {Text, View} from "react-native";
+import {Image, TouchableOpacity} from "react-native";
+import styles from "./screenheader.style";
 
-const ScreenHeaderBtn = () => {
+const ScreenHeaderBtn = ({iconUrl, dimension, handlePress}) => {
   return (
-    <View>
-        <Text>ScreenHeaderBtn</Text>
-    </View>
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+        <Image
+            source={iconUrl}
+            resizeMode="cover"
+            style={styles.btnImg(dimension)}
+        />
+    </TouchableOpacity>
+
   );
 };
 
